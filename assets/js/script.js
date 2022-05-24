032ef6aa0e146dd311f36e871020b45e6b0bd8df
jossparling <jossparling@gmail.com>
2022-05-24 12:29:30 -0500

    added form group

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,11 @@
-var buttonEl = document.querySelector('#save-task');
+var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    listToDoEl.appendChild(listItemEl);
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+  tasksToDoEl.appendChild(listItemEl);
 };
 
-buttonEl.addEventlistener("click", createTaskHandler);
-
+buttonEl.addEventListener("click", createTaskHandler);
